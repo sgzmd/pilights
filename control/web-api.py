eb9ff1209be9ef59342784decfe5b8495746d4c9
Roman Kirillov <sigizmund@gmail.com>
2020-11-24 16:43:34 +0000

    Adding JS for API consumption.

--- control/web-api.py
+++ control/web-api.py
@@ -17,34 +17,48 @@ from algo import algos
 MIN_SPEED = 0
 MAX_SPEED = 100
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=os.path.dirname(__file__) + "/../static")
 ALL_ALGOS = [x.__name__ for x in algos.GetAlgos()]
-current_algo = ALL_ALGOS[0]
+_current_algo = ALL_ALGOS[0]
 _current_speed = 50
+_current_algo_settings = {}
 
 @app.route("/algos", methods=["GET"])
 def all_algos():
   return jsonify([
     {
       'algo': x,
-      'is_current': x == current_algo,
-      'algo_name': x # TODO: switch to x.GetName()
+      'is_current': x == _current_algo,
+      'algo_name': x  # TODO: switch to x.GetName()
     }
-  for x in ALL_ALGOS] )
+    for x in ALL_ALGOS])
 
-@app.route("/set_algo/<algo>", methods=["GET", "POST"])
-def set_algo(algo):
+
+@app.route("/status")
+def status():
+  return jsonify(speed=_current_speed,
+                 min_speed=MIN_SPEED,
+                 max_speed=MAX_SPEED,
+                 current_algo=_current_algo,
+                 current_algo_settings=_current_algo_settings,
+                 algos=ALL_ALGOS)
+
+
+@app.route("/set_algo/<algo>/<color>", methods=["GET", "POST"])
+def set_algo(algo, color=None):
   if not algo in ALL_ALGOS:
     logging.error("Bad algo %s", algo)
     return f"No such algorithm {algo}", 400
   else:
-    current_algo = algo
-    logging.info("Current algorithm is now %s", current_algo)
+    _current_algo = algo
+    logging.info("Current algorithm is now %s with color %s", algo, color)
     return ('', 204)
+
 
 @app.route('/speed', methods=['GET'])
 def get_speed():
   return jsonify(min_speed=MIN_SPEED, max_speed=MAX_SPEED, current_speed=_current_speed)
+
 
 @app.route('/speed/<int:new_speed>', methods=['POST'])
 def set_speed(new_speed):
